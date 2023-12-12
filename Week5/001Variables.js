19f5d821bc879f9c76d975f61f9d45d6c4d86f8d
leoniiip <leonp@missionreadyhq.com>
2023-12-12 19:02:00 +1300

    Variables and Constant Exercise

--- Week5/001Variables.js
+++ Week5/001Variables.js
@@ -1,3 +1,8 @@
-const userName = "Leon Perez";
-console.log("The value in the variable userName is ");
-console.log(userName);
+const firstName = "Leon";
+const lastName = "Perez";
+let fullName;
+
+console.log("The value of first name is " + firstName);
+console.log("The value of last name is " + lastName);
+fullName = ("The value of the full name is " +firstName + " " + lastName);
+console.log(fullName);
