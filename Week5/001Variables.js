136dbb00f10d0e3bd195065924e1fcc46fdf02be
leoniiip <150776926+leoniiip@users.noreply.github.com>
2024-01-30 20:42:03 +1300

    Delete Week5 directory

--- Week5/001Variables.js
+++
@@ -1,8 +0,0 @@
-const firstName = "Leon";
-const lastName = "Perez";
-let fullName;
-
-console.log("The value of first name is " + firstName);
-console.log("The value of last name is " + lastName);
-fullName = ("The value of the full name is " +firstName + " " + lastName);
-console.log(fullName);
